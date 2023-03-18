7087d457d65a5b67d6b328ceb2beac03b7f7044e
omvhadil <fadil.rohman.327@gmail.com>
2023-03-18 16:10:16 +0700

    pwa

--- src/views/madad/Dzikir.vue
+++ src/views/madad/Dzikir.vue
@@ -8,30 +8,22 @@ const category = madad.find((item) => item.slug === categoryId)
 </script>
 <template>
   <div class="mt-3 pb-5">
-    <h4 v-if="category.slug == 'fardhu'" class="text-center mb-4 mt-5">
-      Dzikir Setelah Sholat Fardhu
-    </h4>
-    <h4 v-if="category.slug == 'subuh'" class="text-center mb-4 mt-5">
-      Dzikir Setelah Sholat Subuh
-    </h4>
-    <h4 v-if="category.slug == 'dzuhur'" class="text-center mb-4 mt-5">
-      Dzikir Setelah Sholat Dzuhur
-    </h4>
-    <h4 v-if="category.slug == 'ashar'" class="text-center mb-4 mt-5">
-      Dzikir Setelah Sholat Asar
-    </h4>
-    <h4 v-if="category.slug == 'maghrib'" class="text-center mb-4 mt-5">
-      Dzikir Setelah Sholat Magrib
-    </h4>
-    <h4 v-if="category.slug == 'isya'" class="text-center mb-4 mt-5">
-      Dzikir Setelah Sholat Isya'
-    </h4>
+    <div class="text-center mb-4 mt-5">
+      <h4 v-if="category.slug == 'fardhu'">Dzikir Setelah Sholat Fardhu</h4>
+      <h4 v-if="category.slug == 'subuh'" class="m-0">Dzikir Setelah Sholat Subuh</h4>
+      <h4 v-if="category.slug == 'subuh'" class="m-0">🍀 Wirdul Lathif 🍀</h4>
+      <span v-if="category.slug == 'subuh'" class="m-0">Al-Imam Abdullah Bin Alwi Al-Haddad</span>
+      <h4 v-if="category.slug == 'dzuhur'">Dzikir Setelah Sholat Dzuhur</h4>
+      <h4 v-if="category.slug == 'ashar'">Dzikir Setelah Sholat Asar</h4>
+      <h4 v-if="category.slug == 'maghrib'">Dzikir Setelah Sholat Magrib</h4>
+      <h4 v-if="category.slug == 'isya'">Dzikir Setelah Sholat Isya'</h4>
+    </div>
     <table class="table">
       <tbody>
         <div v-for="item in category.dzikri" :key="item.id">
           <tr v-for="item in item?.bait_bait" :key="item.id">
             <td class="col-11 text-end" :class="item.arab ? 'border-bottom' : ''">
-              <h1 class="m-0 text-white p-3">{{ item.arab }}</h1>
+              <h1 class="m-0 text-white py-3">{{ item.arab }}</h1>
             </td>
           </tr>
         </div>
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,35 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({ registerType: 'autoUpdate',  manifest: {
+      name: 'khairiyah',
+      short_name: 'khairiyah',
+      theme_color: '#198754',
+      background_color: '#198754',
+      display: 'standalone',
+      scope: '/',
+      start_url: '/',
+      lang: 'en',
+      icons: [
+        {
+          src: 'android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    } }) 
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
