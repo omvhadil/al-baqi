53508e2e66e262ade5578b64a82a8f3c745ff88b
omvhadil <fadil.rohman.327@gmail.com>
2023-03-18 14:08:11 +0700

    alert

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
 <script setup>
 import { useRouter } from 'vue-router'
 const router = useRouter()
+const warning = () => {
+  alert('Gi tak mareh')
+}
 </script>
 <template>
   <div class="bg-success bg-gradient p-5 rounded-bottom-5 border-bottom border-5">
@@ -12,36 +15,57 @@ const router = useRouter()
   </div>
   <div class="container mt-4">
     <div class="row row-gap-3">
-      <div class="col-4">
+      <div class="col-4 text-center">
         <div
           @click="router.push('/madad')"
           class="text-center border bg-secondary bg-opacity-25"
-          style="height: 200px"
+          style="height: 170px"
         >
           <img
             src="https://archive.org/services/img/habib-umar-al-khulasoh-2019/full/pct:200/0/default.jpg"
             alt=""
           />
-          <h5>Kholasoh</h5>
         </div>
+        <h5 class="mt-1">Khulasho</h5>
       </div>
-      <div class="col-4">
-        <div class="text-center border bg-secondary bg-opacity-25" style="height: 200px">
+      <div class="col-4 text-center">
+        <div
+          @click="warning()"
+          class="text-center border bg-secondary bg-opacity-25"
+          style="height: 170px"
+        >
           <img
             src="https://www.sinarbarualgensindo.com/wp-content/uploads/2020/06/Majmu-luks.jpg"
             alt=""
           />
-          <h5>Majmu'</h5>
         </div>
+        <h5 class="mt-1">Majmu'</h5>
       </div>
-      <div class="col-4">
-        <div class="text-center border bg-secondary bg-opacity-25" style="height: 200px">
+      <div class="col-4 text-center">
+        <div
+          @click="warning()"
+          class="text-center border bg-secondary bg-opacity-25"
+          style="height: 170px"
+        >
           <img
             src="https://blogger.googleusercontent.com/img/a/AVvXsEhU-qxdBiMy4mHbK6mQj6Sf3JLNFxO5CtN_Xb5wKuWGwwQxY_bxSMUqEf1Tc7v3iYZVdanFZrK-GSbIIm2g7h06krtNBAf-7A_Q6FoDd7guk5iUhBakTUGHIBBZbc5AM4iwnjlkpkJ5BRDzfYQviDAgj5KJmpEND8XdRqW-oWdlKPvPwUR8WUH1Psmw=w480-h640"
             alt=""
           />
-          <h5>Simtut</h5>
         </div>
+        <h5 class="mt-1">Simtut</h5>
+      </div>
+      <div class="col-4 text-center">
+        <div
+          @click="warning()"
+          class="text-center border bg-secondary bg-opacity-25"
+          style="height: 170px"
+        >
+          <img
+            src="https://3.bp.blogspot.com/-FNVBvyNGxCk/V1dbhEH2F2I/AAAAAAAABs8/Kv6bNAsleXkUbggcQBA8oQ6lFrpHaxUGgCLcB/s1600/download-pdf-keutamaan-puasa-karya-ibnu-hajar-haitami.jpg"
+            alt=""
+          />
+        </div>
+        <h5 class="mt-1">Romadhon</h5>
       </div>
     </div>
   </div>
