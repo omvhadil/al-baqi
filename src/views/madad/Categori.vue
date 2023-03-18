9fc08e532ee040f8fdcf9e59775fe14c4ed00824
omvhadil <fadil.rohman.327@gmail.com>
2023-03-18 10:37:08 +0700

    vercel

--- src/views/madad/Categori.vue
+++ src/views/madad/Categori.vue
@@ -1,18 +1,15 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
-import { data } from '../../constans/index.js'
+import { madad } from '../../constans/index.js'
 </script>
 <template>
-  <div v-for="item in data" :key="item.id" class="row row-gap-4 mt-3">
-    <router-link
-      class="col-12 nav-link"
-      v-for="item in item.category"
-      :key="item.id"
-      :to="item.link"
-    >
-      <div class="border p-3 shadow-sm rounded d-flex align-items-center justify-content-center">
-        <h4 class="m-0">{{ item.title }}</h4>
-      </div>
-    </router-link>
+  <div v-for="item in madad" :key="item.id" class="row row-gap-4 mt-3">
+    <div class="col-12">
+      <router-link class="nav-link" :to="item.link">
+        <div class="border p-3 shadow-sm rounded d-flex align-items-center justify-content-center">
+          <h4 class="m-0">{{ item.title }}</h4>
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
--- src/views/madad/Fardhu.vue
+++ src/views/madad/Fardhu.vue
@@ -1,20 +1,18 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
-import { data } from '../../constans/index.js'
+import { madad } from '../../constans/index.js'
 </script>
 <template>
   <div class="mt-3">
-    <h4 class="m-0 mb-3 text-center">Dzikir Setelah Sholat Fardhu</h4>
-    <div v-for="item in data" :key="item.id">
-      <table v-for="item in item.category" :key="item.id" class="table table-striped">
-        <tbody v-for="item in item.dzikri" :key="item.id">
-          <tr v-for="item in item.bait_bait" :key="item.id">
-            <td class="col-11 text-end">
-              <h2 class="m-0">{{ item.arab }}</h2>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <h4 class="text-center mb-4">Dzikir Setelah Sholat Fardhu</h4>
+    <table v-for="item in madad" :key="item.id" class="table table-striped">
+      <tbody v-for="item in item.dzikri" :key="item.id">
+        <tr v-for="item in item.bait_bait" :key="item.id">
+          <td class="col-11 text-end">
+            <h2 class="m-0">{{ item.arab }}</h2>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
